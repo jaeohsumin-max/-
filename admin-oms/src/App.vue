091c6d5fd7d8a5937fd97b05df9597814889a8b4
sumin <jaeohsumin@naver.com>
2026-06-04 11:31:33 +0900

    Add transparent CODEMUSE logo to OMS admin header.

--- admin-oms/src/App.vue
+++ admin-oms/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-grey-10 text-white">
-      <q-toolbar>
-        <q-toolbar-title class="text-weight-medium">
-          CODEMUSE 백오피스
-        </q-toolbar-title>
+      <q-toolbar class="q-px-md">
+        <img
+          :src="logoUrl"
+          alt="CODEMUSE"
+          class="header-logo q-mr-sm"
+        />
+        <q-space />
         <q-chip dense color="grey-8" text-color="white" icon="inventory_2">
           OMS
         </q-chip>
@@ -21,4 +24,20 @@
 
 <script setup>
 import OrderManagement from "./pages/OrderManagement.vue";
+import logoUrl from "./assets/codemuse-logo.png";
 </script>
+
+<style scoped>
+.header-logo {
+  height: 36px;
+  width: auto;
+  display: block;
+  object-fit: contain;
+}
+
+@media (min-width: 600px) {
+  .header-logo {
+    height: 44px;
+  }
+}
+</style>
